ea47159da9cc9851600645aa144e6557c3a0d529
tecktrio <amalpullan4@gmail.com>
2025-01-20 13:39:40 +0530

    updates

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ wss.on('connection', (ws) => {
 });
 
 
-app.listen(PORT, "192.168.1.5",() => {
+app.listen(PORT, "0.0.0.0",() => {
     console.log(`Server is running on http://localhost:${PORT}`);
 
     // Automatically open the browser
